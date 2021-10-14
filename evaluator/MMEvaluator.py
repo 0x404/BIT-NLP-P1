459057138fadd2f02ed350050e3fd54059b71b03
0x404 <871206929@qq.com>
2021-10-14 16:04:13 +0800

    将函数换到其他文件

--- evaluator/MMEvaluator.py
+++ evaluator/MMEvaluator.py
@@ -1,66 +1,14 @@
 '''
 author: 0x404
 Date: 2021-10-14 13:48:35
-LastEditTime: 2021-10-14 15:48:26
+LastEditTime: 2021-10-14 16:02:39
 Description: 
 '''
+
 import time
+import tools.dataLoader as dataLoader
 import seg.MM as MM
-
-def getIntervals(text):
-    """
-    返回分词结果的区间表示
-    :param text: 分词后的列表
-    :return: 区间表示的列表
-    """
-    ans = []
-    sumL = 0
-    for w in text:
-        ans.append((sumL, sumL + len(w) - 1))
-        sumL = sumL + len(w)
-    return ans
-
-def getIntersectCount(x, y):
-    counterX, counterY = {}, {}
-    for interval in x:
-        if interval not in counterX.keys():
-            counterX[interval] = 0
-        else:
-            counterX[interval] += 1
-    for interval in y:
-        if interval not in counterY.keys():
-            counterY[interval] = 0
-        else:
-            counterY[interval] += 1
-    ans = 0
-    for key in counterX.keys():
-        if key in counterY.keys():
-            ans += 1
-    return ans
-
-def dataLoader(n, path):
-    """
-    加载测试集
-    :param n: 测试集中的句子数
-    :param path: 测试集路径
-    :return: (testData, ansData)
-    """
-    file = open(path, mode="r", encoding="utf-8")
-    testData, ansData = [], []
-    for id, line in enumerate(file):
-        if id >= n:
-            break
-        line = line.strip()
-        ansItem = line.split("  ")
-        testItem = ""
-        for c in line:
-            if c != " ":
-                testItem = testItem + c
-        testData.append(testItem)
-        ansData.append(getIntervals(ansItem))
-        
-    file.close()
-    return testData, ansData
+import util.helper as helper
 
 def evaluate(mode, path, n, dic):
     """
@@ -76,7 +24,7 @@ def evaluate(mode, path, n, dic):
     if mode not in ("FMM", "RMM", "BMM"):
         raise Exception("最长匹配算法：模式不存在")
     
-    testData, ansData = dataLoader(n, path)
+    testData, ansData = dataLoader.loadTestData(n, path)
     
     resMM = []
     if mode == "FMM":
@@ -88,21 +36,20 @@ def evaluate(mode, path, n, dic):
     
     predData = []
     for item in resMM:
-        predData.append(getIntervals(item))
+        predData.append(helper.getIntervals(item))
     
     sizeA, sizeB, sizeIntersect = 0, 0, 0
 
     for i in range(len(ansData)):
         sizeA += len(ansData[i])
         sizeB += len(predData[i])
-        sizeIntersect += getIntersectCount(ansData[i], predData[i])
+        sizeIntersect += helper.getIntersectCount(ansData[i], predData[i])
     
     p = sizeIntersect / sizeB
     r = sizeIntersect / sizeA
     f = 2 * p * r / (p + r)
 
     print ("使用{}算法进行测试，测试集规模为{}, 测试共进行{:.2f}s".format(mode, n, time.time() - startTime))
-
     return p, r, f
 
 
