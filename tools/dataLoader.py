2085cb4570956276a898ec01b073ad88a704432d
0x404 <871206929@qq.com>
2021-10-14 16:04:31 +0800

    添加测试集加载函数

--- tools/dataLoader.py
+++ tools/dataLoader.py
@@ -1,10 +1,10 @@
 '''
 author: 0x404
 Date: 2021-10-13 20:12:16
-LastEditTime: 2021-10-14 13:32:33
+LastEditTime: 2021-10-14 16:01:23
 Description: 
 '''
-
+import util.helper as helper
 
 def loadDictionary(path):
     '''读取字典文件，返回单词列表'''
@@ -13,16 +13,30 @@ def loadDictionary(path):
     for line in file:
         line = line.split('	')
         wordList.append(line[0])
+    file.close()
     return wordList
 
-def loadTestSet(path):
+def loadTestData(n, path):
     """
-    读取测试集
-    :return: [[sentenc1], [sentence2], ... ]
+    加载测试集
+    :param n: 测试集中的句子数
+    :param path: 测试集路径
+    :return: (testData, ansData)
     """
     file = open(path, mode="r", encoding="utf-8")
-    result = []
-    for line in file:
-        result.append(line)
-    return result
+    testData, ansData = [], []
+    for id, line in enumerate(file):
+        if id >= n:
+            break
+        line = line.strip()
+        ansItem = line.split("  ")
+        testItem = ""
+        for c in line:
+            if c != " ":
+                testItem = testItem + c
+        testData.append(testItem)
+        ansData.append(helper.getIntervals(ansItem))
+        
+    file.close()
+    return testData, ansData
 
