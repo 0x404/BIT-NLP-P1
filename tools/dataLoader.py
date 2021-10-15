1b385cb3c3517cdb1cbc291271a599155a961813
0x404 <871206929@qq.com>
2021-10-15 14:59:22 +0800

    增加词性标记数据加载函数

--- tools/dataLoader.py
+++ tools/dataLoader.py
@@ -1,7 +1,7 @@
 '''
 author: 0x404
 Date: 2021-10-13 20:12:16
-LastEditTime: 2021-10-14 20:50:32
+LastEditTime: 2021-10-15 14:40:29
 Description: 
 '''
 import util.helper as helper
@@ -18,7 +18,7 @@ def loadDictionary(path):
 
 def loadSegData(n, path):
     """
-    加载测试集
+    加载分词测试集
     :param n: 测试集中的句子数
     :param path: 测试集路径
     :return: (testData, ansData)
@@ -39,4 +39,25 @@ def loadSegData(n, path):
         
     file.close()
     return testData, ansData
+
+def loadPosData(n, path):
+    """
+    加载词性标注数据集
+    :param n: 加载的句子数
+    :param path: 数据集路径
+    :return: [[sentence1], [sentence2], ... ]   sentence = [{"text":word1, "tag":tag1}, {"text":word2, "tag":tag2}, ...]
+    """
+    file = open(path, mode="r", encoding="utf-8")
+    posData = []
+    for id, line in enumerate(file):
+        if id >= n:
+            break
+        line = line.strip()
+        line = line.split("	")
+        data = []
+        for word in line:
+            pos = word.rfind("/", 0, len(word))
+            data.append({"text" : word[0 : pos], "tag" : word[pos + 1 : ]})
+        posData.append(data)
+    return posData
 
