0e664b8896e222fe4826459c1e8c55eb1af4ab1f
0x404 <871206929@qq.com>
2021-10-16 20:02:35 +0800

    添加基于HMM的NER识别

---
+++ ner/HMM.py
@@ -0,0 +1,167 @@
+'''
+author: 0x404
+Date: 2021-10-16 18:41:14
+LastEditTime: 2021-10-16 19:59:34
+Description: 
+'''
+
+import numpy as np
+import algorithm
+
+def loadHMMNERData(path, n = 100000):
+    """
+    加载使用HMM进行NER任务的数据
+    :param path: BMEO标签数据路径
+    :param n: 加载的句子数量
+    :return: [sentence1, sentence2, ... ]    sentence = [{"text" : word1, "tag", tag1}, {"text" : word2, "tag", tag2}]
+    """
+    file = open(path, mode="r", encoding="utf-8")
+    nerData = []
+    for line in file:
+        line = line.strip()
+        line = line.split("	")
+        sentence = []
+        for w in line:
+            w = w.split("/")
+            sentence.append({"text" : w[0], "tag" : w[1]})
+        nerData.append(sentence)
+    return nerData[0 : n]
+
+def normalize(matrix):
+    """
+    先对矩阵进行归一化，后对矩阵取对数
+    :param matrix: 待归一化的矩阵
+    :return: 处理后的矩阵
+    """
+    for i in range(len(matrix)):
+        tot = sum(cnt for cnt in matrix[i])
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 0:
+                matrix[i][j] = float('-INF')            # 如果概率为0，取对数后为负无穷
+            else:
+                matrix[i][j] = matrix[i][j] / tot
+                matrix[i][j] = np.log(matrix[i][j])     # 否则归一化后取对数
+    return matrix
+    
+def generateBegin(samples, tagID):
+    """
+    训练初始矩阵
+    :param samples: 训练集[[sample1], [sample2], ... ]
+    :param tagID: map[tag]->id
+    :return: 训练完成的初始矩阵
+    """
+    begin = [0 for i in range(len(tagID))]
+    for sample in samples:
+        start = sample[0]
+        begin[tagID[start["tag"]]] += 1
+        
+    begin = normalize([begin])[0]
+    return begin
+
+def generateTrans(samples, tagID):
+    """
+    训练转移矩阵
+    :param samples: 训练集[[sample1], [sample2], ... ]
+    :param tagID: map[tag]->id
+    :return: 训练完成的转移矩阵
+    """
+    trans = [[0 for i in range(len(tagID))] for _ in range(len(tagID))]
+    for sample in samples:
+        if len(sample) < 2:
+            continue
+        prev = sample[0]
+        for j in range(1, len(sample)):
+            now = sample[j]
+            trans[tagID[prev["tag"]]][tagID[now["tag"]]] += 1
+            prev = now
+    
+    trans = normalize(trans)
+    return trans
+
+def generateEmit(samples, tagID):
+    """
+    训练发射矩阵
+    :param samples: 训练集[[sample1], [sample2], ... ]
+    :param tagID: map[tag]->id
+    :return: 训练完成的转移矩阵
+    """
+    emit = [{} for i in range(len(tagID))] # 出于性能考虑，emit[i]是一个map而不是一个连续数组
+    for sample in samples:
+        for data in sample:
+            text = data["text"]
+            tag = data["tag"]
+            if text not in emit[tagID[tag]].keys():
+                emit[tagID[tag]][text] = 1
+            else:
+                emit[tagID[tag]][text] += 1
+    
+    # 由于使用map，无法直接调用normalize归一化
+    for i in range(len(emit)):
+        tot = 0
+        for j in emit[i].keys():
+            tot += emit[i][j]
+        for j in emit[i].keys():
+            if emit[i][j] == 0:
+                emit[i][j] = float('-INF')
+            else:
+                emit[i][j] = emit[i][j] / tot
+                emit[i][j] = np.log(emit[i][j])
+    
+    return emit
+
+def generateTagMap():
+    tagId = {"B_nr" : 0, "B_ns" : 1, "B_nt" : 2,
+             "M_nr" : 3, "M_ns" : 4, "M_nt" : 5,
+             "E_nr" : 6, "E_ns" : 7, "E_nt" : 8,
+             "S_nr" : 9, "S_ns" : 10, "S_nt" : 11,
+             "O": 12}
+    idTag = {}
+    for key in tagId.keys():
+        idTag[tagId[key]] = key
+    return tagId, idTag
+
+def decoder(input):
+    """
+    对viterbi算法的结果进行解码
+    :param input: viterbi算法的结果, 形如['中共中央/B_nt', '政治局/E_nt', '常委/O', '李岚清/S_nr']
+    :return: 人名集合nrSet, 地名nsSet, 机构名ntSet
+    """
+    f = {"nr" : 0, "ns" : 1, "nt" : 2}
+    result = [[], [], []]
+    
+
+    for i in range(len(input)):
+        input[i] = input[i].split("/")
+
+    i = 0
+    while i < len(input):
+        text = input[i][0]
+        tag = input[i][1]
+        if tag in ("S_nr", "S_nt", "S_ns"):
+            result[f[tag[2:]]].append(text)
+            i += 1
+            continue
+        elif tag in ("B_nr", "B_nt", "B_ns"):
+            now = ""
+            while input[i][1][0] != "E":
+                now += input[i][0]
+                i += 1
+            now += input[i][0]
+            result[f[input[i][1][2:]]].append(now)
+        i += 1
+    return result[0], result[1], result[2]
+
+
+if __name__ == "__main__":
+    samples = loadHMMNERData("..\\data\\ner-processed\\199801-train.txt")
+    tagId, idTag = generateTagMap()
+    begin = generateBegin(samples, tagId)
+    trans = generateTrans(samples, tagId)
+    emit = generateEmit(samples, tagId)
+    ans = algorithm.viterbi(["福建省"], begin, trans, emit, tagId, idTag)
+    name, location, orgnization = decoder(ans)
+    print (ans)
+    print ("人名：", name)
+    print ("地名：", location)
+    print ("机构名：", orgnization)
+    
---
+++ ner/algorithm.py
@@ -0,0 +1,73 @@
+'''
+author: 0x404
+Date: 2021-10-15 15:14:01
+LastEditTime: 2021-10-16 16:05:43
+Description: 
+'''
+
+def viterbi(observation, begin, trans, emit, tagId, idTag):
+    """
+    利用维特比算法，计算词性标注
+    :param observation: 待标注的句子，如["寂静", "如", "雾", "缓缓", "漫", "开"]
+    :param begin: HMM初始概率向量
+    :param trans: HMM转移矩阵
+    :param emit:  HMM发射矩阵
+    :param tagId: tag到正数下标的map
+    :param idTag: 正数下标到tag的map
+    :return: 标注结果，如["寂静/a", "如/v", "雾/n", "缓缓/d", "漫/v", "开/v"]
+    """
+    t = len(observation)
+
+    dp = [[float('-INF') for i in range(len(tagId))] for _ in range(2)]     # dp[i][j]表示到第i层当前状态为j时的最大概率，考虑dp[i][j]仅与dp[i-1][k]有关，故使用滚动数组
+    ls = [[-1 for i in range(len(tagId))] for _ in range(t)]                # ls[i][j]为当前再第i层，状态为j，上一步由i - 1的哪个状态转移而来
+    
+    for i in range(len(tagId)):
+        if observation[0] in emit[i].keys():
+            dp[0][i] = begin[i] + emit[i][observation[0]]
+        else:
+            dp[0][i] = begin[i] + float('-10000')     # 如果状态i到第一个词的发射概率为0，则记为负无穷
+    
+    for i in range(t - 1):
+        pre, now = i, i + 1
+
+        for j in range(len(tagId)): 
+            dp[now & 1][j] = float('-INF')  # 清空当前dp数组
+
+        for preS in range(len(tagId)):  # 枚举前一个状态
+            for nowS in range(len(tagId)):  # 枚举当前状态
+
+                prob = 0    # 计算转移概率
+                if observation[now] not in emit[nowS].keys():
+                    prob = dp[pre & 1][preS] + trans[preS][nowS] + float('-10000')
+                else:
+                    prob = dp[pre & 1][preS] + trans[preS][nowS] + emit[nowS][observation[now]]
+
+                if prob > dp[now & 1][nowS]:
+                    dp[now & 1][nowS] = prob    #更新dp的最大值
+                    ls[now][nowS] = preS        #维护ls数组
+    
+    ans = ["" for i in range(t)]    # 答案标签
+
+    maxProb = float('-INF')
+    state = -1
+    for i in range(len(tagId)):     
+        if dp[(t - 1) & 1][i] >= maxProb:   # 找到最后一个位置概率最大的状态
+            maxProb = dp[(t - 1) & 1][i]
+            state = i
+    
+    i = t - 1
+    while i >= 0 and state != -1:   # 从后往前，通过ls数组逆推，得到答案标签数组
+        ans[i] = idTag[state]
+        state = ls[i][state]
+        i -= 1
+    
+    res = []
+    for i in range(len(ans)):
+        res.append(observation[i] + "/" + ans[i])
+    return res
+
+
+
+
+
+                
